0f5710dd41ec3aa3c60aaa64cca5b2e4af982380
Musa Khan <tamzid101@gmail.com>
2017-08-18 15:16:50 +0600

    main.sql updated.

--- main.sql
+++ main.sql
@@ -9,6 +9,10 @@ CREATE TABLE IF NOT EXISTS `admin` (
   PRIMARY KEY (`id`)
 ) AUTO_INCREMENT=2 ;
 
+INSERT INTO `admin` (`userName`, `password`) VALUES
+('root', 'password');
+
+
 --
 -- Dumping data for table `admin`
 --
@@ -37,24 +41,44 @@ CREATE TABLE IF NOT EXISTS `employee` (
 ) AUTO_INCREMENT=2 ;
 
 
-CREATE TABLE IF NOT EXISTS `ssc_info` (
+
+
+CREATE TABLE IF NOT EXISTS `exam_info` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `employee_id` int(11) NOT NULL ,
   `year` varchar(50) NOT NULL,
   `board` varchar(50) NOT NULL,
   `roll` varchar(50) NOT NULL,
   `reg` varchar(50) NOT NULL,
+  `exam_name` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
 ) AUTO_INCREMENT=2 ;
 
 
-CREATE TABLE IF NOT EXISTS `hsc_info` (
+CREATE TABLE IF NOT EXISTS `university_link` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `employee_id` int(11) NOT NULL ,
-  `year` varchar(50) NOT NULL,
-  `board` varchar(50) NOT NULL,
-  `roll` varchar(50) NOT NULL,
-  `reg` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `link` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) AUTO_INCREMENT=2 ;
+
+
+CREATE TABLE IF NOT EXISTS `university_info` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Other_students` varchar(255) ,
+  `Campus` varchar(255) ,
+  `Undergraduates` varchar(255) ,
+  `Postgraduates` varchar(255) ,
+  `Doctoral_students` varchar(255) ,
+  `Website` varchar(255) ,
+  `Motto_in_English` varchar(255) ,
+  `Academic_staff` varchar(255) ,
+   `TYPE` varchar(255) ,
+  `Students` varchar(255) ,
+  `Vice_Chancellor` varchar(255) ,
+  `Established` varchar(255) ,
+  `Location` varchar(255) ,
+  `uni_id` int(11) ,
   PRIMARY KEY (`id`)
 ) AUTO_INCREMENT=2 ;
 
